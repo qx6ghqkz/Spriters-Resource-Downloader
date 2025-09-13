84adeacf68e548683ed1e0fe15aa9fda145f7137
qx6ghqkz <93668667+qx6ghqkz@users.noreply.github.com>
2025-09-13 13:29:29 +0100

    Support downloading NSFW assets

--- spriters-resource-downloader.py
+++ spriters-resource-downloader.py
@@ -8,10 +8,11 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("url", help="URL to a game on the website spriters-resource.com")
 parser.add_argument("-v", "--verbose", action="store_true", help="use it to print progress during download")
+parser.add_argument("--nsfw", action="store_true", help="use it to enable downloading of NSFW assets")
 
 args = parser.parse_args()
 
-downloader = SpritersResourceTools.SpritersResourceDownloader(args.url)
+downloader = SpritersResourceTools.SpritersResourceDownloader(args.url, args.nsfw)
 if args.verbose:
     downloader.setVerboseEnabled(True)
 
